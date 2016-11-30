9ac1a3202080dbeb7b70f75bc4a2ed4b850e5061
Luke Woydziak <luke.woydziak@emc.com>
2016-11-30 15:24:01 -0800

    use invoker in nfsdriver to standardize CLI access [#135247503](https://www.pivotaltracker.com/story/show/135247503)

--- cephbroker/client.go
+++ cephbroker/client.go
@@ -7,12 +7,12 @@ import (
 	"path/filepath"
 
 	"code.cloudfoundry.org/cephbroker/utils"
-	"code.cloudfoundry.org/goshims/execshim"
 	"code.cloudfoundry.org/goshims/ioutilshim"
 	"code.cloudfoundry.org/goshims/osshim"
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/voldriver"
 	"code.cloudfoundry.org/voldriver/driverhttp"
+	"code.cloudfoundry.org/voldriver/invoker"
 )
 
 //go:generate counterfeiter -o ../cephfakes/fake_ceph_client.go . Client
@@ -28,7 +28,7 @@ type Client interface {
 
 type cephClient struct {
 	mds                 string
-	invoker             Invoker
+	invoker             invoker.Invoker
 	os                  osshim.Os
 	ioutil              ioutilshim.Ioutil
 	baseLocalMountPoint string
@@ -44,7 +44,7 @@ var (
 	KeyringNotFound error = errors.New("unable to open cephfs keyring")
 )
 
-func NewCephClientWithInvokerAndSystemUtil(mds string, useInvoker Invoker, os osshim.Os, ioutil ioutilshim.Ioutil, localMountPoint string, keyringFile string) Client {
+func NewCephClientWithInvokerAndSystemUtil(mds string, useInvoker invoker.Invoker, os osshim.Os, ioutil ioutilshim.Ioutil, localMountPoint string, keyringFile string) Client {
 	return &cephClient{
 		mds:                 mds,
 		invoker:             useInvoker,
@@ -58,7 +58,7 @@ func NewCephClientWithInvokerAndSystemUtil(mds string, useInvoker Invoker, os os
 func NewCephClient(mds string, localMountPoint string, keyringFile string, remoteMountPath string) Client {
 	return &cephClient{
 		mds:                 mds,
-		invoker:             NewRealInvoker(),
+		invoker:             invoker.NewRealInvoker(),
 		os:                  &osshim.OsShim{},
 		ioutil:              &ioutilshim.IoutilShim{},
 		baseLocalMountPoint: localMountPoint,
@@ -158,51 +158,6 @@ func (c *cephClient) invokeCeph(env voldriver.Env, args []string) error {
 	cmd := "ceph-fuse"
 	logger.Info("invoking-ceph", lager.Data{"cmd": cmd, "args": args})
 	defer logger.Debug("done-invoking-ceph")
-	return c.invoker.Invoke(driverhttp.EnvWithLogger(logger, env), cmd, args)
-}
-
-//go:generate counterfeiter -o ../cephfakes/fake_invoker.go . Invoker
-
-type Invoker interface {
-	Invoke(env voldriver.Env, executable string, args []string) error
-}
-
-type realInvoker struct {
-	useExec execshim.Exec
-}
-
-func NewRealInvoker() Invoker {
-	return NewRealInvokerWithExec(&execshim.ExecShim{})
-}
-
-func NewRealInvokerWithExec(useExec execshim.Exec) Invoker {
-	return &realInvoker{useExec}
-}
-
-func (r *realInvoker) Invoke(env voldriver.Env, executable string, cmdArgs []string) error {
-	logger := env.Logger().Session("invoke")
-	logger.Info("start")
-	defer logger.Info("end")
-
-	cmdHandle := r.useExec.CommandContext(env.Context(), executable, cmdArgs...)
-
-	_, err := cmdHandle.StdoutPipe()
-	if err != nil {
-		logger.Error("unable-to-get-stdout", err)
-		return err
-	}
-
-	if err = cmdHandle.Start(); err != nil {
-		logger.Error("starting-command", err)
-		return err
-	}
-
-	if err = cmdHandle.Wait(); err != nil {
-
-		logger.Error("command-exited", err)
-		return err
-	}
-
-	// could validate stdout, but defer until actually need it
-	return nil
+	_, err := c.invoker.Invoke(driverhttp.EnvWithLogger(logger, env), cmd, args)
+	return err
 }
--- cephbroker/client_test.go
+++ cephbroker/client_test.go
@@ -1,20 +1,16 @@
 package cephbroker_test
 
 import (
-	"bytes"
-	"fmt"
-
 	"context"
 
 	"code.cloudfoundry.org/cephbroker/cephbroker"
-	"code.cloudfoundry.org/cephbroker/cephfakes"
-	"code.cloudfoundry.org/goshims/execshim/exec_fake"
 	"code.cloudfoundry.org/goshims/ioutilshim/ioutil_fake"
 	"code.cloudfoundry.org/goshims/osshim/os_fake"
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/lager/lagertest"
 	"code.cloudfoundry.org/voldriver"
 	"code.cloudfoundry.org/voldriver/driverhttp"
+	"code.cloudfoundry.org/voldriver/voldriverfakes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -25,7 +21,7 @@ var CephClient = Describe("CephClient", func() {
 		ctx         context.Context
 		env         voldriver.Env
 		subject     cephbroker.Client
-		fakeInvoker *cephfakes.FakeInvoker
+		fakeInvoker *voldriverfakes.FakeInvoker
 		fakeOs      *os_fake.FakeOs
 		fakeIoutil  *ioutil_fake.FakeIoutil
 	)
@@ -33,7 +29,7 @@ var CephClient = Describe("CephClient", func() {
 		logger = lagertest.NewTestLogger("test-broker")
 		ctx = context.TODO()
 		env = driverhttp.NewHttpDriverEnv(logger, ctx)
-		fakeInvoker = &cephfakes.FakeInvoker{}
+		fakeInvoker = &voldriverfakes.FakeInvoker{}
 		fakeOs = &os_fake.FakeOs{}
 		fakeIoutil = &ioutil_fake.FakeIoutil{}
 		subject = cephbroker.NewCephClientWithInvokerAndSystemUtil("mds", fakeInvoker, fakeOs, fakeIoutil, "localMountPoint", "keyringFile")
@@ -72,54 +68,6 @@ var CephClient = Describe("CephClient", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(detail1).To(Equal("mds"))
 			Expect(detail2).To(Equal(""))
-		})
-	})
-})
-
-var Invoker = Describe("RealInvoker", func() {
-	var (
-		subject    cephbroker.Invoker
-		fakeCmd    *exec_fake.FakeCmd
-		fakeExec   *exec_fake.FakeExec
-		testLogger = lagertest.NewTestLogger("InvokerTest")
-		testCtx    = context.TODO()
-		testEnv    voldriver.Env
-		cmd        = "some-fake-command"
-		args       = []string{"fake-args-1"}
-	)
-	Context("when invoking an executable", func() {
-		BeforeEach(func() {
-			testEnv = driverhttp.NewHttpDriverEnv(testLogger, testCtx)
-			fakeExec = new(exec_fake.FakeExec)
-			fakeCmd = new(exec_fake.FakeCmd)
-			fakeExec.CommandContextReturns(fakeCmd)
-			subject = cephbroker.NewRealInvokerWithExec(fakeExec)
-		})
-
-		It("should report an error when unable to attach to stdout", func() {
-			fakeCmd.StdoutPipeReturns(errCloser{bytes.NewBufferString("")}, fmt.Errorf("unable to attach to stdout"))
-			err := subject.Invoke(testEnv, cmd, args)
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("unable to attach to stdout"))
-		})
-
-		It("should report an error when unable to start binary", func() {
-			fakeCmd.StdoutPipeReturns(errCloser{bytes.NewBufferString("cmdfails")}, nil)
-			fakeCmd.StartReturns(fmt.Errorf("unable to start binary"))
-			err := subject.Invoke(testEnv, cmd, args)
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("unable to start binary"))
-		})
-		It("should report an error when executing the driver binary fails", func() {
-			fakeCmd.WaitReturns(fmt.Errorf("executing driver binary fails"))
-
-			err := subject.Invoke(testEnv, cmd, args)
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("executing driver binary fails"))
-		})
-		It("should successfully invoke cli", func() {
-			err := subject.Invoke(testEnv, cmd, args)
-			Expect(err).ToNot(HaveOccurred())
 		})
 	})
 })
